a694fc17f63ee56daf0000638a30554506e54b16
Jacob Witz <>
2022-02-15 20:49:24 +0000

    Update version banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -207,7 +207,7 @@ module.exports = {
         lastVersion: 'current',
         versions: {
         current: {
-          label: '0.27',
+          label: '0.28',
           path: '',
           banner: 'none',
          },
