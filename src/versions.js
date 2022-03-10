b2cd94ec896963cccf5f71dd9369ce4a340c01f7
Paola <paola@astronomer.io>
2022-03-10 17:59:06 -0500

    update versions.js file with new runtime version

--- src/versions.js
+++ src/versions.js
@@ -1,6 +1,6 @@
 export const siteVariables = {
   cliVersion: '1.3.0',
-  runtimeVersion: '4.1.0',
+  runtimeVersion: '4.2.0',
   // Hacky variable so that we can use env var fromatting in CI/CD templates
   deploymentid: '${DEPLOYMENT_ID}',
 };
