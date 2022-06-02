8f6f6b84e9a6900200f746b66fdeffce976e8c99
jwitz <jwitz@astronomer.io>
2022-06-02 13:27:47 -0600

    Hotfix envar

--- src/versions.js
+++ src/versions.js
@@ -1,5 +1,5 @@
 export const siteVariables = {
-  cliVersion: '1.5.0',
+  cliVersion: '1.0.0',
   runtimeVersion: '5.0.1',
   // Hacky variable so that we can use env var formatting in CI/CD templates
   deploymentid: '${ASTRONOMER_DEPLOYMENT_ID}',
