a6ad9735abbbff9920718c3f821b5f828a1d9ffb
Paola Peraza Calderon <paola@astronomer.io>
2022-06-05 23:24:09 -0400

    Update versions file to Runtime 5.0.3, now latest

--- src/versions.js
+++ src/versions.js
@@ -1,6 +1,6 @@
 export const siteVariables = {
   cliVersion: '1.0.0',
-  runtimeVersion: '5.0.1',
+  runtimeVersion: '5.0.3',
   // Hacky variable so that we can use env var formatting in CI/CD templates
   deploymentid: '${ASTRONOMER_DEPLOYMENT_ID}',
   keyid: '${ASTRONOMER_KEY_ID}',
