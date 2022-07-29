429433f9e067f72db193dc8fc96fcb6ae9caadb9
jwitz <jwitz@astronomer.io>
2022-07-29 08:41:44 +0100

    Add maintenance tag to versions doc - hotfix

--- src/versions.js
+++ src/versions.js
@@ -1,13 +1,11 @@
 export const siteVariables = {
-  // version-specifc
+  // version-specific
   cliVersion: '1.2.0',
-  runtimeVersion: '5.0.6',
+  runtimeVersion: '5.0.4',
   // Hacky variable so that we can use env var formatting in CI/CD templates
   deploymentid: '${ASTRONOMER_DEPLOYMENT_ID}',
   keyid: '${ASTRONOMER_KEY_ID}',
   keysecret: '${ASTRONOMER_KEY_SECRET}',
   // Hacky variable for droneci
   deploymentiddrone: '$ASTRONOMER_DEPLOYMENT_ID',
-  // Hacky variable for Jenkins multibranch
-  jenkinsenv: '${env.GIT_BRANCH}',
 };
