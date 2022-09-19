f57e5c78ac185901fdb57d5739057958e984aa29
jwitz <jwitz@astronomer.io>
2022-09-19 15:42:57 +0100

    Update sidebars

--- sidebarsTutorials.js
+++ sidebarsTutorials.js
@@ -1,16 +1,92 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
- The sidebars can be generated from the filesystem, or explicitly defined here.
- Create as many sidebars as you want.
- */
-
 module.exports = {
+  concepts: [
+    {
+      type: 'category',
+      label: 'Airflow basics',
+      items: [
+        'intro-to-airflow',
+        'what-is-an-operator',
+        'what-is-a-sensor',
+        'connections',
+        'what-is-a-hook',
+        'dags',
+        'airflow-decorators',
+        'bashoperator',
+        'airflow-sql',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Airflow infrastructure',
+      items: [
+        'airflow-database',
+        'airflow-executors-explained',
+        'airflow-components',
+        'deferrable-operators',
+        'using-airflow-plugins',
+
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Operationalize Airflow',
+      items: [
+        'custom-xcom-backends',
+        'logging',
+        'kubepod-operator',
+        'testing-airflow',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'DAG design and best practices',
+      items: [
+          'airflow-passing-data-between-tasks',
+          'airflow-branch-operator',
+          'airflow-pools',
+          'airflow-scaling-workers',
+          'dag-best-practices',
+          'airflow-importing-custom-hooks-operators',
+          'debugging-dags',
+          'subdags',
+          'task-groups',
+          'rerunning-dags',
+          'managing-dependencies',
+          'managing-airflow-code',
+          'templating',
+          'dynamically-generating-dags',
+      ],
+    },
+  ],
   tutorials: [
-    'overview',
+    'overview'
     'get-started-with-airflow',
-    'astro-python-sdk',
+    {
+      type: 'category',
+      label: 'DAG writing tutorials',
+      items: [
+        'astro-python-sdk',
+        'execute-notebooks',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Integrations',
+      items: [
+        'overview',
+        'airflow-azure-data-factory-integration',
+        'airflow-azure-data-explorer',
+        'airflow-openlineage',
+        'airflow-dbt',
+        'airflow-databricks',
+        'airflow-sagemaker',
+        'airflow-redshift',
+        'airflow-great-expectations',
+        'airflow-talend-integration',
+        'airflow-sql-data-quality-tutorial',
+        'astro-python-sdk-etl',
+        'soda-data-quality',
+      ],
+    },
   ],
 };
