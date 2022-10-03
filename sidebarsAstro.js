8f2e6cf819728cf9fc2ff547a4a751864159123a
jwitz <jwitz@astronomer.io>
2022-10-03 15:13:15 +0100

    hotfix double command

--- sidebarsAstro.js
+++ sidebarsAstro.js
@@ -208,7 +208,6 @@ module.exports = {
       'cli/astro-context-delete',
       'cli/astro-context-list',
       'cli/astro-context-switch',
-      'cli/astro-completion',
       'cli/astro-deploy',
       'cli/astro-deployment-create',
       'cli/astro-deployment-delete',
