9df003364829be383f2761865166e3f8d48a2d7e
Jake Witz <74574233+jwitz@users.noreply.github.com>
2023-01-04 04:26:49 -0700

    Refactor Software archive (#1658)
    * Create new home for unmaintained Software documentaton
    
    * Add archive to sidebar nav
    
    * Add redirects
    
    * Fix splate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ module.exports = {
         {
           label: '0.30',
           to: '/software/0.30/',
-          activeBaseRegex: `software(?!(\/${versions.join('|\\/')}))`,
+          activeBaseRegex: '(software\/0.30)+',
         },
         {
           label: '0.29',
@@ -124,6 +124,11 @@ module.exports = {
           label: '0.25',
           to: '/software/0.25/overview',
           activeBaseRegex: '(software\/0.25)+',
+        },
+        {
+          label: 'Archive',
+          to: '/software/documentation-archive',
+          activeBaseRegex: `software(?!(\/${versions.join('|\\/')}))`,
         },
       ],
     },
